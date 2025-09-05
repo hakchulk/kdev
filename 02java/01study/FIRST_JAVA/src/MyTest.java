133fd3991284348bdd08acc779510f1bcb6a2e0d
hakchulk <hakchulk@gmail.com>
2025-09-05 18:24:59 +0900

    '넷마블'

---
+++ 02java/01study/FIRST_JAVA/src/MyTest.java
@@ -0,0 +1,8 @@
+public class MyTest {
+    public static void main(String[] args) {
+        byte b=1;
+        int num = 1234567890;
+        System.out.println("hello java!!" + b + num);
+        
+    }
+}
